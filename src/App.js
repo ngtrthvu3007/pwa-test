4c17c60f17ccc614c3edf99bf3eb78ae24bd116f
nguyentranthevu <103686648+ngtrthvu3007@users.noreply.github.com>
2024-04-11 10:52:11 +0700

    custom install btn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+      const handler = (e) => {
+          e.preventDefault();
+          setSupportsPWA(true);
+          setPromptInstall(e);
+      };
+      window.addEventListener('beforeinstallprompt', handler);
+  }, []
+  )
+
+  const onInstallClick = () => {
+    if (!supportsPWA) {
+        alert(
+            'Either you have already installed the app or your browser does not support PWA :('
+        );
+        return;
+    }
+    promptInstall.prompt();
+};
+
+const renderInstallOption = () => {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        return;
+    } else {
+        return (
+            <div id="custom-install-prompt">
+                <img src="https://example.com/pwa-icon.png" alt="PWA Icon"/>
+                <h2>Install MyApp</h2>
+                <p>Get instant access to MyApp from your home screen.</p>
+                <button onClick={onInstallClick} id="install-btn">Install</button>
+                <button id="dismiss-btn">Dismiss</button>
+            </div>
+        );
+    }
+};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +58,7 @@ function App() {
         >
           Learn React
         </a>
+        <div className="">{renderInstallOption()}</div>
       </header>
     </div>
   );
